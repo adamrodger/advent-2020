5d91dbf8d30af11263c0890412eef44cdad4fe23
Adam Rodger <adam.rodger@gmail.com>
2020-12-07 09:45:05 +0000

    Day 7 🍪

---
+++ src/AdventOfCode/Day7.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 7
+    /// </summary>
+    public class Day7
+    {
+        public int Part1(string[] input)
+        {
+            var rules = ParseRules(input);
+
+            HashSet<string> possible = new HashSet<string>();
+
+            foreach (string start in rules.Keys)
+            {
+                IEnumerable<string> chain = Chain(start, rules);
+                if (chain.Contains("shiny gold"))
+                {
+                    possible.Add(start);
+                }
+            }
+
+            return possible.Count - 1; // discount the shiny gold bag itself
+        }
+
+        public int Part2(string[] input)
+        {
+            var rules = ParseRules(input);
+            return CountBags(0, "shiny gold", rules) - 1; // discount the shiny gold bag itself
+        }
+
+        private static IEnumerable<string> Chain(string current, Dictionary<string, Dictionary<string, int>> bags)
+        {
+            yield return current;
+
+            foreach (string next in bags[current].Keys)
+            {
+                foreach (var x in Chain(next, bags))
+                {
+                    yield return x;
+                }
+            }
+        }
+
+        private static int CountBags(int total, string current, Dictionary<string, Dictionary<string, int>> bags)
+        {
+            Dictionary<string, int> targets = bags[current];
+
+            int newTotal = total;
+
+            foreach ((string target, int count) in targets)
+            {
+                newTotal += CountBags(total, target, bags) * count;
+            }
+
+            newTotal += 1; // count current bag
+            return newTotal;
+        }
+
+        private static Dictionary<string, Dictionary<string, int>> ParseRules(string[] input)
+        {
+            // Examples:
+            //     shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+            //     faded blue bags contain no other bags.
+
+            Regex bagRegex = new Regex(@"(\d+) ([a-z ]+)+ bags?", RegexOptions.Singleline | RegexOptions.Compiled);
+            Dictionary<string, Dictionary<string, int>> rules = new Dictionary<string, Dictionary<string, int>>();
+
+            foreach (string line in input)
+            {
+                string[] split = line.Split(new[] { " bags contain " }, StringSplitOptions.None);
+
+                string colour = split[0];
+                rules[colour] = new Dictionary<string, int>();
+
+                if (line.EndsWith("no other bags."))
+                {
+                    continue;
+                }
+
+                foreach (string target in split[1].Split(','))
+                {
+                    Match match = bagRegex.Match(target);
+                    int amount = int.Parse(match.Groups[1].Value);
+                    string targetColour = match.Groups[2].Value;
+
+                    rules[colour][targetColour] = amount;
+                }
+            }
+
+            return rules;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day7Tests.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day7Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day7 solver;
+
+        public Day7Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day7();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day7.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                /*"shiny gold bags contain 2 dark red bags.",
+                "dark red bags contain 2 dark orange bags.",
+                "dark orange bags contain 2 dark yellow bags.",
+                "dark yellow bags contain 2 dark green bags.",
+                "dark green bags contain 2 dark blue bags.",
+                "dark blue bags contain 2 dark violet bags.",
+                "dark violet bags contain no other bags."*/
+
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                "faded blue bags contain no other bags.",
+                "dotted black bags contain no other bags."
+            };
+        }
+
+        [Fact]
+        public void Part1_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = 4;
+
+            var result = solver.Part1(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 242;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 7 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            //var expected = 126;
+            var expected = 32;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 176035;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 7 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
