27791a2ed3c3d4f321fb679386e274374041f623
Adam Rodger <adam.rodger@gmail.com>
2020-12-10 09:04:48 +0000

    Day 10 🕯️

---
+++ src/AdventOfCode/Day10.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 10
+    /// </summary>
+    public class Day10
+    {
+        public int Part1(string[] input)
+        {
+            var numbers = input.Select(int.Parse).ToList();
+
+            int max = numbers.Max();
+            int oneDiff = 0, threeDiff = 1;
+            int current = 0;
+
+            while (current < max)
+            {
+                var next = numbers.Where(n => n <= current + 3).Min();
+                numbers.Remove(next);
+
+                if (next == current + 1)
+                {
+                    oneDiff++;
+                }
+                else if (next == current + 3)
+                {
+                    threeDiff++;
+                }
+
+                current = next;
+            }
+
+            return oneDiff * threeDiff;
+        }
+
+        public long Part2(string[] input)
+        {
+            var numbers = input.Select(int.Parse).OrderByDescending(n => n).ToList();
+            numbers.Add(0);
+            int max = numbers.Max();
+            int target = max + 3;
+
+            var paths = new Dictionary<int, long> { { target, 1 } };
+
+            // walk backwards (instead of forwards!) through the numbers and work out how many different ways there were to get to each one
+            foreach (int current in numbers)
+            {
+                paths.TryGetValue(current + 1, out long oneDiff);
+                paths.TryGetValue(current + 2, out long twoDiff);
+                paths.TryGetValue(current + 3, out long threeDiff);
+                paths[current] = oneDiff + twoDiff + threeDiff;
+            }
+
+            return paths[0];
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day10Tests.cs
@@ -0,0 +1,117 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day10Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day10 solver;
+
+        public Day10Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day10();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day10.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                /*"16",
+                "10",
+                "15",
+                "5",
+                "1",
+                "11",
+                "7",
+                "19",
+                "6",
+                "12",
+                "4"*/
+
+                "28",
+                "33",
+                "18",
+                "42",
+                "31",
+                "14",
+                "46",
+                "20",
+                "48",
+                "47",
+                "24",
+                "23",
+                "49",
+                "45",
+                "19",
+                "38",
+                "39",
+                "11",
+                "1",
+                "32",
+                "25",
+                "35",
+                "8",
+                "17",
+                "7",
+                "9",
+                "4",
+                "2",
+                "34",
+                "10",
+                "3"
+            };
+        }
+
+        [Fact]
+        public void Part1_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = 220;
+
+            var result = solver.Part1(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 2738;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 10 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = 19208;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            // 274877906944 -- too low
+            var expected = 74049191673856;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 10 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
