ddfa1856e683a6ced043c29571c136e6a97df979
Adam Rodger <adam.rodger@gmail.com>
2020-12-13 08:15:00 +0000

    Day 13 🧦

---
+++ src/AdventOfCode/Day13.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 13
+    /// </summary>
+    public class Day13
+    {
+        public int Part1(string[] input)
+        {
+            int start = int.Parse(input[0]);
+            int time = start;
+            var buses = input[1].Numbers<int>();
+
+            while (true)
+            {
+                int leaving = buses.FirstOrDefault(b => time % b == 0);
+
+                if (leaving != default)
+                {
+                    return (time - start) * leaving;
+                }
+
+                time++;
+            }
+        }
+
+        public long Part2(string[] input)
+        {
+            string[] strings = input[1].Split(',');
+            var buses = new List<int>();
+            var diffs = new Dictionary<int, int>();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string s = strings[i];
+
+                if (s == "x")
+                {
+                    continue;
+                }
+
+                int bus = int.Parse(s);
+                buses.Add(bus);
+                diffs[bus] = i;
+            }
+
+            long time = 0;
+            long multiplier = 1;
+
+            // basically, find the LCM between each bus and the previous ones (which satisfies the offset)
+            // and advance time by that much until you've done all buses
+            foreach (int bus in buses)
+            {
+                int offset = diffs[bus];
+
+                while ((time + offset) % bus != 0)
+                {
+                    // jump by the multiplier until you find a time where this bus syncs with the previous ones
+                    time += multiplier;
+                }
+
+                // include this bus into the jumps to keep everything in sync
+                multiplier *= bus;
+            }
+
+            return time;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day13Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day13Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day13 solver;
+
+        public Day13Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day13();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day13.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 3997;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 13 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            long expected = 500033211739354;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 13 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
