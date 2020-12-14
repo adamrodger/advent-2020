87526ebb8409b2214aef5f3308ec1d5d199bf65a
Adam Rodger <adam.rodger@gmail.com>
2020-12-14 13:12:14 +0000

    Day 14 🥛

---
+++ src/AdventOfCode/Day14.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 14
+    /// </summary>
+    public class Day14
+    {
+        public long Part1(string[] input)
+        {
+            Dictionary<long, long> registers = new Dictionary<long, long>();
+            string mask = "";
+
+            foreach (string line in input)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    mask = line.Substring(6).Trim();
+                    continue;
+                }
+
+                var nums = line.Numbers<long>();
+                long address = nums[0];
+
+                string valueBits = ApplyMask(mask, nums[1]);
+                long value = Convert.ToInt64(valueBits, 2);
+
+                registers[address] = value;
+            }
+
+            return registers.Values.Sum();
+        }
+
+        public long Part2(string[] input)
+        {
+            Dictionary<long, long> registers = new Dictionary<long, long>();
+            string mask = "";
+
+            foreach (string line in input)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    mask = line.Substring(6).Trim();
+                    continue;
+                }
+
+                var nums = line.Numbers<long>();
+                long value = nums[1];
+
+                string addressMask = ApplyMask(mask, nums[0], true);
+                ICollection<string> addresses = MultiplyMask(addressMask).ToList();
+
+                foreach (string floating in addresses)
+                {
+                    long address = Convert.ToInt64(floating, 2);
+                    registers[address] = value;
+                }
+            }
+
+            return registers.Values.Sum();
+        }
+
+        private static string ApplyMask(string mask, long value, bool part2 = false)
+        {
+            string valueBits = Convert.ToString(value, 2).PadLeft(36, '0');
+
+            IEnumerable<char> chars = mask.Zip(valueBits, (m, v) => part2
+                                                                ? m != '0' ? m : v      // mask | value
+                                                                : m != 'X' ? m : v);    // mask overwrites, X passes value bit through
+
+            return new string(chars.ToArray());
+        }
+
+        private static IEnumerable<string> MultiplyMask(string mask)
+        {
+            if (!mask.Contains('X'))
+            {
+                return new[] { mask };
+            }
+
+            var zero = ReplaceFirst(mask, 'X', '0');
+            var one = ReplaceFirst(mask, 'X', '1');
+
+            // branch and recurse
+            return MultiplyMask(zero).Concat(MultiplyMask(one));
+        }
+
+        private static string ReplaceFirst(string mask, char old, char replacement)
+        {
+            int index = mask.IndexOf(old);
+
+            var sb = new StringBuilder(mask);
+            sb.Remove(index, 1);
+            sb.Insert(index, replacement);
+            return sb.ToString();
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day14Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day14Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day14 solver;
+
+        public Day14Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day14();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day14.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 14862056079561;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 14 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 3296185383161;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 14 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
