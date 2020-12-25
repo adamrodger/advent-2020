555584870e891f3483d2a28f0d634785a2a1123a
Adam Rodger <adam.rodger@gmail.com>
2020-12-25 05:38:36 +0000

    Day 25 🎅🤶🦌🍪🥛🍷🌟❄️☃️🎄🎁

---
+++ src/AdventOfCode/Day25.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 25
+    /// </summary>
+    public class Day25
+    {
+        public long Part1(string[] input)
+        {
+            long door = int.Parse(input[0]);
+            long card = int.Parse(input[1]);
+
+            long value = 1;
+            int loop = 0;
+            
+            while (value != door)
+            {
+                value = (value * 7) % 20201227;
+                loop++;
+            }
+
+            value = 1;
+
+            for (int i = 0; i < loop; i++)
+            {
+                value = (value * card) % 20201227;
+            }
+
+            return value;
+        }
+    }
+}
